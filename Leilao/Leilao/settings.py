db168b7aa2c4d8fc7f4a2e3ea756ee6dc9eb5c6b
Allan Oliveira <allanoliveira@outlook.com>
2021-10-22 17:15:44 -0300

    Added bootstrap

--- Leilao/Leilao/settings.py
+++ Leilao/Leilao/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'bids',
     'auctions',
     'registration',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -82,21 +83,21 @@ WSGI_APPLICATION = 'Leilao.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = { 
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 'NAME': 'dbleilao',
-        'USER': 'root',
-        'PASSWORD': 'Tati159951+',
-        'HOST': 'localhost', 'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#DATABASES = { 
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', 'NAME': 'dbleilao',
+#        'USER': 'root',
+#        'PASSWORD': 'Tati159951+',
+#        'HOST': 'localhost', 'PORT': '3306',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
