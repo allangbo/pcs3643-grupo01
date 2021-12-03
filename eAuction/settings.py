23b1db76dffc0bc1e97df4837320253425ff9095
Allan Oliveira <allanoliveira@outlook.com>
2021-12-03 19:26:50 -0300

    #up

--- eAuction/settings.py
+++ eAuction/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 'NAME': 'dbleilao',
         'USER': 'root',
         'PASSWORD': 'admin',
-        'HOST': 'localhost', 'PORT': '3306',
+        'HOST': 'db', 'PORT': '3306',
     }
 }
 
