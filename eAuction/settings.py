41b75016b727ca00916d7b86d40ff8c290359395
Allan Oliveira <allanoliveira@outlook.com>
2021-12-03 17:16:38 -0300

    Updated settings and docker compose test

--- eAuction/settings.py
+++ eAuction/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 'NAME': 'dbleilao',
         'USER': 'root',
         'PASSWORD': 'admin',
-        'HOST': 'localhost', 'PORT': '3306',
+        'HOST': '*', 'PORT': '3306',
     }
 }
 
