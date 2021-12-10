ff73a4572b462b805110ae8d94f8c5adaeb83c58
Allan Oliveira <allanoliveira@outlook.com>
2021-12-09 22:44:06 -0300

    #up

--- eAuction/urls.py
+++ eAuction/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('batches/', include('batches.urls')),  
     path('reports/', include('reports.urls')),  
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
